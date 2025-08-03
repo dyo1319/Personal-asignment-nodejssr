94939b620ffa2675a3435e36ae5d1d036d87c603
dyo1319 <raad_asded@hotmail.com>
2025-08-03 21:01:59 +0300

    added simple style.css for a href-btn

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+
 const bodyParser = require('body-parser');
 const path = require("path");
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'style')));
 
 var cookieParser = require('cookie-parser');
 app.use(cookieParser());
